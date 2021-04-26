51950a62419f12479e69412bc980918b8c029e73
Riderlord <riderlordtv@gmail.com>
2021-04-26 15:15:15 +0530

    pdf2audio done

--- pdf2audio.py
+++ pdf2audio.py
@@ -5,6 +5,6 @@ pdfreader = PyPDF2.PdfFileReader(open('test1.pdf', 'rb'))
 speaker = pyttsx3.init()
 for num in range(pdfreader.numPages):
     text = pdfreader.getPage(num).extractText()
-    speaker.say(Text)
+    speaker.say(text)
     speaker.runAndWait()
 speaker.stop()
